45c2af253d69b17f595153e4262953599115ffb4
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2023-02-18 21:58:48 +0530

    Update gojang5.go

--- gojang5.go
+++ gojang5.go
@@ -13,6 +13,21 @@ type Point struct {
     x, y float64
 }
 
+// The Point struct represents a 2D point with x and y coordinates.
+//
+// The crossProduct function calculates the cross product of two vectors formed by three points, which is used to determine the orientation of the points 
+// (clockwise or counterclockwise).
+//
+// The grahamScan function takes a slice of Points and returns a slice representing the Convex Hull using the Graham Scan algorithm. It first finds the 
+// point with the smallest y-coordinate (ties broken by smallest x-coordinate) to use as the anchor point. It then sorts the remaining points by polar 
+// angle with respect to the anchor point using the sortByPolarAngle function. The sorted points are then processed in order to build the Convex Hull. 
+// Starting with the first two points, the algorithm scans through the remaining points and adds them to the hull if they turn left (i.e., the cross 
+// product is positive). If they turn right (cross product is negative or zero), it removes the most recently added point from the hull until the new point 
+// can be added without turning right. The result is a slice of Points representing the Convex Hull.
+//
+// In the main function, we create a slice of Points, use the grahamScan function to compute the Convex Hull, and print the resulting points to the console.
+//
+
 func crossProduct(p, q, r Point) float64 {
     return (q.x-p.x)*(r.y-p.y) - (q.y-p.y)*(r.x-p.x)
 }
